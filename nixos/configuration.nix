44dccf70fe527ceee88cc28365a7d97cbcffc242
ShaddyDC <shaddythefirst@gmail.com>
2022-12-20 22:11:46 +0100

    Add more tools

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -143,6 +143,19 @@
   environment.systemPackages = with pkgs; [
     vim
     wget
+    unzip
+    vlc
+    mpv
+    zathura
+    okular
+    htop
+    du-dust
+    ripgrep
+    youtube-dl
+    gcc
+    pandoc
+    gdb
+
     rclone
     keepassxc
     zellij
@@ -162,13 +175,15 @@
     nil
   ];
 
+  programs.command-not-found.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
