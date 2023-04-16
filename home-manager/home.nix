8bba797a0893f29ce54fc50bc35e312986257466
ShaddyDC <shaddythefirst@gmail.com>
2023-04-16 12:33:47 +0200

    Disable games and wine

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -65,10 +65,10 @@
       zotero
       zoom-us
       minigalaxy
-      inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin
-      inputs.nix-gaming.packages.${pkgs.system}.osu-stable
-      inputs.nix-gaming.packages.${pkgs.system}.wine-discord-ipc-bridge
-      inputs.nix-gaming.packages.${pkgs.system}.wine-ge
+       inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin
+#       inputs.nix-gaming.packages.${pkgs.system}.osu-stable
+#       inputs.nix-gaming.packages.${pkgs.system}.wine-discord-ipc-bridge
+#       inputs.nix-gaming.packages.${pkgs.system}.wine-ge
 
       # yubikey-manager-qt
     ];
