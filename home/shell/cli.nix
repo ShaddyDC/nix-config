0d6ae5e65eea951c3486c84aa2706d7dd4aaec32
Shaddy <shaddythefirst@gmail.com>
2023-06-28 08:41:52 +0200

    Enable and style helix

--- home/shell/cli.nix
+++ home/shell/cli.nix
@@ -45,7 +45,39 @@
       enable = true;
       defaultEditor = true;
     };
-    helix.enable = true;
+    helix = {
+      enable = true;
+      settings = {
+        theme = "catppuccin_mocha";
+
+        editor = {
+          color-modes = true;
+          cursorline = true;
+          cursor-shape = {
+            insert = "bar";
+            normal = "block";
+            select = "underline";
+          };
+          indent-guides = {
+            render = true;
+            rainbow-option = "dim";
+          };
+          lsp.display-inlay-hints = true;
+          # rainbow-brackets = true;
+          statusline.center = ["position-percentage"];
+          true-color = true;
+          whitespace.characters = {
+            newline = "↴";
+            tab = "⇥";
+          };
+        };
+        keys.normal.space.u = {
+          f = ":format"; # format using LSP formatter
+          w = ":set whitespace.render all";
+          W = ":set whitespace.render none";
+        };
+      };
+    };
 
     zellij = {
       enable = true;
