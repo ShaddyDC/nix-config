5e019ab42c7c05681afc404cf59da51017a28ba6
ShaddyDC <shaddythefirst@gmail.com>
2023-07-23 22:24:48 +0200

    Update some hashes (to be fixed properly)

--- home/programs/discocss.nix
+++ home/programs/discocss.nix
@@ -4,7 +4,7 @@
   xdg.configFile."discocss/custom.css" = {
     source = pkgs.fetchurl {
       url = "https://catppuccin.github.io/discord/dist/catppuccin-mocha.theme.css";
-      hash = "sha256-4n2tkJy4jkoMuTQnk0ZNFxm8UPrbKrTLBXVVEAY2Y1s=";
+      hash = "sha256-QmoUPzi4dApswZiGr8Jj2bGaGbiUHpqth+0qxlhMSPA=";
     };
   };
 }
