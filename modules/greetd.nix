88c5782e96f0f20b8317990a0704b7817ca5b0c9
Shaddy <shaddythefirst@gmail.com>
2023-12-14 15:57:43 +0100

    greetd: clean up unsed code

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -1,30 +1,8 @@
 {
-  lib,
   pkgs,
-  config,
   default,
   ...
 }:
-# greetd display manager
-# let
-  # greetdSwayConfig = pkgs.writeText "greetd-sway-config" ''
-  #   exec "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP"
-  #   input "type:touchpad" {
-  #     tap enabled
-  #   }
-  #   seat seat0 xcursor_theme Bibata-Modern-Classic 24
-  #   xwayland disable
-
-  #   bindsym Mod4+shift+e exec swaynag \
-  #     -t warning \
-  #     -m 'What do you want to do?' \
-  #     -b 'Poweroff' 'systemctl poweroff' \
-  #     -b 'Reboot' 'systemctl reboot'
-
-  #   # exec "${lib.getExe config.programs.regreet.package} -l debug; swaymsg exit"
-  #   exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
-  # '';
-# in 
 {
   environment.systemPackages = with pkgs; [
     # theme packages
@@ -53,12 +31,6 @@
     };
   };
 
-  # programs.sway.enable = true;
-  # services.xserver.displayManager.sddm.enable = false;
-
-  # services.greetd.settings.default_session.command =
-  #   "${config.programs.sway.package}/bin/sway --config ${greetdSwayConfig}"
-  #   + (lib.optionalString (config.networking.hostName == "rog") " --unsupported-gpu");
 
   # # unlock GPG keyring on login
   security.pam.services.greetd.enableGnomeKeyring = true;
