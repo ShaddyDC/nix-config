e0a6c7c500a3c6ca8c62cc992bcea9f9d4ed9658
Shaddy <shaddythefirst@gmail.com>
2023-12-15 09:22:29 +0100

    packages: Fix obsidian

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,7 @@
 {
   self,
   inputs,
+  lib,
   ...
 }: {
   systems = ["x86_64-linux"];
@@ -14,7 +15,14 @@
       ];
     });
 
-   obsidian_oop = prev.obsidian.override { electron = prev.electron_26; };
+    obsidian_oop = lib.throwIf (lib.versionOlder "1.4.16" prev.obsidian.version) "Obsidian no longer requires EOL Electron" (
+      prev.obsidian.override {
+        electron = prev.electron_25.overrideAttrs (_: {
+          preFixup = "patchelf --add-needed ${prev.libglvnd}/lib/libEGL.so.1 $out/bin/electron"; # NixOS/nixpkgs#272912
+          meta.knownVulnerabilities = []; # NixOS/nixpkgs#273611
+        });
+      }
+    );
   };
 
   perSystem = {pkgs, ...}: {
