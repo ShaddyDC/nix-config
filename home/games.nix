e5d5cff0062e8f9d4a0764c7edc3a3359dd2288f
ShaddyDC <shaddythefirst@gmail.com>
2024-06-02 16:24:03 +0200

    games: Disable osu lazer

--- home/games.nix
+++ home/games.nix
@@ -5,9 +5,9 @@
 }: {
   home.packages = with pkgs; [
     minigalaxy
-    inputs'.nix-gaming.packages.osu-lazer-bin
+    # inputs'.nix-gaming.packages.osu-lazer-bin
     # inputs'.nix-gaming.packages.osu-stable
-    inputs'.nix-gaming.packages.wine-discord-ipc-bridge
+    # inputs'.nix-gaming.packages.wine-discord-ipc-bridge
     #       inputs'.nix-gaming.packages..wine-ge
     gamescope
   ];
